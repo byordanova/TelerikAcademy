c07578f525b6c3f0474a443ea744118d87a90d27
Yordanova <bn.ynova@gmail.com>
2013-04-21 21:29:56 +0300

    Revert "First commit"
    This reverts commit a752ef3915bd4ed8cfd7a95fe1c22c8d61366191.

--- Homework_01_CMS/404.php
+++
@@ -1,9 +0,0 @@
-<?php
-get_header(); ?>
-
-      <div class="content">
-        <h1>Page Not Found</h1>  
-		<p>Sorry, but the page you requested has not been found</p>  
-      </div>
-
-<?php get_footer(); ?>
